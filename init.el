d06b113e9f60c5e8498b14bdfb1d25f85d3b61e0
AlexFilip <AlexFilipTech@pm.me>
2025-08-03 17:04:34 -0400

    Add package management functions

--- init.el
+++ init.el
@@ -1,6 +1,23 @@
 ;;; -*- lexical-binding: t; -*-
 (setq custom-file "~/.config/emacs/custom.el")
-(if (file-exists-p custom-file) (load-file custom-file))
+
+(defun some (fn list)
+  "CUSTOM implementation of some"
+  (let ((f (if (symbolp fn)
+	       (symbol-function fn)
+	     fn)))
+    (if (funcall f (car list))
+	t
+      (some f (cdr list)))))
+
+(defun all (fn list)
+  "CUSTOM implementation of all"
+  (let ((f (if (symbolp fn)
+	       (symbol-function fn)
+	     fn)))
+    (if (funcall f (car list))
+	(all f (cdr list))
+      t)))
 
 (defun require-pkg (package-name)
   (unless (package-installed-p package-name)
@@ -8,13 +25,21 @@
     (package-install package-name))
   (require package-name))
 
+(defun require-pkgs (&rest package-names)
+  (unless (all 'package-installed-p package-names)
+      (progn
+	(package-refresh-contents)
+	(dolist (pkg package-names)
+	  (unless (package-installed-p pkg)
+	    (package-install pkg)))))
+  (dolist (pkg package-names) (require pkg)))
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
 (package-initialize)
 
-(require-pkg 'naysayer-theme)
-(require-pkg 'gruber-darker-theme)
+(require-pkgs 'naysayer-theme 'gruber-darker-theme)
 (load-theme 'naysayer t)
 
 (add-to-list 'custom-theme-load-path (expand-file-name "~/.config/emacs/themes/"))
@@ -26,10 +51,14 @@
 (setq backup-directory-alist `(("." . auto-save-file-directory)))
 (setq backup-by-copying t)
 
-(setq inhibit-startup-message t)
-(setq inhibit-startup-screen t)
-(setq initial-scratch-message nil)
+(setq-default
+ inhibit-startup-message t
+ inhibit-startup-screen t
+ initial-scratch-message nil)
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+
+(when (file-exists-p custom-file)
+  (load-file custom-file))
