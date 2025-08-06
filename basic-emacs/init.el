70c2ab6bdaec3e770c6eb6a6241c1a9849536cd1
AlexFilip <AlexFilipTech@pm.me>
2025-08-06 09:36:28 -0400

    Allow menu bar on mac

--- basic-emacs/init.el
+++ basic-emacs/init.el
@@ -60,7 +60,7 @@
  initial-scratch-message nil)
 
 (tool-bar-mode -1)
-(menu-bar-mode -1)
+(unless (eq system-type 'darwin) (menu-bar-mode -1))
 (scroll-bar-mode -1)
 
 (when (file-exists-p custom-file)
