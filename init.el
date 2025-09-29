891af37a275f9ce31d45d1859937ac856e831c85
AlexFilip <AlexFilipTech@pm.me>
2025-09-28 21:06:05 -0400

    Add some org commands and ignore eshell

--- init.el
+++ init.el
@@ -1,34 +1,21 @@
 ;;; -*- lexical-binding: t; -*-
 (setq custom-file "~/.config/emacs/custom.el")
 
-(defun all (fn list)
-  "CUSTOM implementation of all"
-  (if (null list)
-      t
-    (let ((f (if (symbolp fn)
-		 (symbol-function fn)
-	       fn)))
-      (if (funcall f (car list))
-	  (all f (cdr list))
-	nil))))
-
-(defun require-pkgs (&rest package-names)
-  (unless (all 'package-installed-p package-names)
-    (package-refresh-contents)
-    (dolist (pkg package-names)
-      (unless (package-installed-p pkg)
-	(package-install pkg))))
-  (dolist (pkg package-names) (require pkg)))
-
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
 (package-initialize)
 
 ;; Packages
-(require-pkgs 'bluetooth)
-(require-pkgs 'clojure-mode 'slime)
+(use-package bluetooth :ensure t)
 
+(use-package clojure-mode :ensure t)
+(use-package slime :ensure t)
+
+(use-package magit :ensure t)
+
+(use-package elfeed :ensure t)
+(use-package elfeed-org :ensure t)
 ;; Theming
 (add-to-list 'custom-theme-load-path (expand-file-name "~/.config/emacs/themes/"))
 (load-theme 'custom t)
@@ -59,6 +46,25 @@
 
 (ido-mode 1)
 ; (ido-everywhere 1)
+
+;; Org-mode
+(setq org-directory (expand-file-name "~/notes.org/"))
+
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+
+;; Org capture templates from https://orgmode.org/manual/Capture-templates.html
+;; TODO: Make this load from a template directory
+(setq org-default-notes-file (concat org-directory "captures.org")
+      org-capture-templates
+      `(("t" "Todo" entry (file+headline ,(concat org-directory "test.org") "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("j" "Journal" entry (file+datetree ,(concat org-directory "journal.org"))
+         "* %?\nEntered on %U\n  %i\n  %a")
+	("b" "Bookmark" entry (file+headline ,(concat org-directory "test.org") "Bookmarks")
+         "* %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n" :empty-lines 1)))
+
 
 (when (file-exists-p custom-file)
   (load-file custom-file))
